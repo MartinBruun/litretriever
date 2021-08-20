6fa5f159e2e082464ea6dd089ef17831c8db4f5a
emil8560 <ebm@hotmail.dk>
2021-08-20 14:13:58 +0200

    Ups.. NU er den fixet (ikke så easy)
    Havde glemt at commit

---
+++ litretriever/AUB_API.py
@@ -0,0 +1,10 @@
+import requests
+
+response = requests.get("https://kbdk-aub.primo.exlibrisgroup.com")
+
+# Test connection
+if response.status_code == 200:
+    print("A.Okay")
+else:
+    print("no no no")
+
